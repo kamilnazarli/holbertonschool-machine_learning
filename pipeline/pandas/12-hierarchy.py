422fa576c447770fe85f490279cf35c44446758d
Kamil <nezerovk425@gmail.com>
2026-01-07 16:51:28 +0400

    To concatenate two dataframes

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+This module creates a function
+to concatenate 2 dataframes
+'''
+import pandas as pd
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    '''
+    this function concatenates
+    two dataframes
+    '''
+    df1 = index(df1)
+    df2 = index(df2)
+    df2 = df2[(df2.index >= 1417411980)& (df2.index <= 1417417980)]
+    return pd.concat([df2, df1], keys=['bitstamp', 'coinbase'],/
+        levels=['Timestamp']).sort_index()
