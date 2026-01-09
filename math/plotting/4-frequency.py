c6acf5875e50f713a81458abe0606beb08178101
Kamil <nezerovk425@gmail.com>
2026-01-09 16:51:04 +0400

    To make histogram

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''make histogram'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    '''to make histogram'''
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.title('Project A')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.hist(student_grades, bins=10, range=(0, 101), edgecolor='black')
