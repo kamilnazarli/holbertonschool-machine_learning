056458db11ebb02af04d357ff5b2b92d1e916c17
Kamil <nezerovk425@gmail.com>
2026-01-09 17:30:54 +0400

    To make histogram

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+''docstring'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    '''doc''
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
+    categories = ['Farrah', 'Fred', 'Felicia']
+    apples, bananas, oranges, peaches = fruit[0], fruit[1], fruit[2], fruit[3]
+    plt.title('Number of Fruit per Person')
+    plt.ylabel('Quantity of Fruit')
+    plt.ylim(0, 80)
+    plt.bar(categories, apples, color='red', width=0.5)
+    plt.bar(categories, bananas, bottom=apples, color='yellow', width=0.5)
+    plt.bar(categories, oranges, bottom=apples+bananas, color='#ff8000', width=0.5)
+    plt.bar(categories, peaches, bottom=apples+bananas+oranges, color='#ffe5b4', width=0.5)
+    plt.legend(['apples', 'bananas', 'oranges', 'peaches'])
