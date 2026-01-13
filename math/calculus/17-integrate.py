0c2aa748a3e92e7a6849d5cd52ed491a9a471a02
Kamil <nezerovk425@gmail.com>
2026-01-13 15:34:16 +0400

    find integrated version

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -6,9 +6,11 @@ def poly_integral(poly, C=0):
     ''' integration '''
     if type(poly) is not list or len(poly) == 0:
         return None
-    if len(poly) == 1:
-        return [0]
     ls = []
+    if len(poly) == 1:
+        ls.append(poly[0])
+        ls.insert(0, C)
+        return ls
     for i in range(len(poly)):
         if poly[i]/(i+1) == poly[i]//(i+1):
             ls.append(poly[i]//(i+1))
