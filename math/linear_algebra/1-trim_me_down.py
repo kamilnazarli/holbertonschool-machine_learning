e1b9e94004d58b613d0ea240e46b3c18ba4afb76
Kamil <nezerovk425@gmail.com>
2026-01-20 10:32:19 +0000

    slicing again

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -2,5 +2,6 @@
 import numpy as np
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
-the_middle = np.array(matrix)[::, 2:4]
+for row in matrix:
+    the_middle.append(row[2:4])
 print("The middle columns of the matrix are: {}".format(the_middle))
