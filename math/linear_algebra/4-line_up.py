30bea4c929882265176eadde860ffc2837f7ffa6
kamil <nezerovk425@gmail.com>
2026-01-23 15:26:29 +0000

    element-wise sum

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 '''module documented'''
-
 def matrix_shape(matrix):
     '''function1 documented'''
     shape = []
@@ -17,10 +16,10 @@ def add_arrays(arr1, arr2):
     '''function2 documented'''
     if len(arr1) == 0 and len(arr2) == 0:
         return None
-    elif shape1 != shape2:
-        return None
     shape1 = matrix_shape(arr1)
     shape2 = matrix_shape(arr2)
+    if shape1 != shape2:
+        return None
     result = []
     for i in range(len(arr1)):
         result.append(arr1[i]+arr2[i])
