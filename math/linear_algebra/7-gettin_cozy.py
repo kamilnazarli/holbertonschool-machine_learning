36ae2749f2ffa2fe0a06d5be582c4d0c51d7e162
Kamil <nezerovk425@gmail.com>
2026-01-25 15:48:21 +0000

    Concat

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -2,24 +2,17 @@
 '''module documented'''
 
 
-def matrix_shape(matrix):
-    '''function1 documented'''
-    shape = []
-    current = matrix
-    while True:
-        if type(current) is list and len(current) > 0:
-            shape.append(len(current))
-            current = current[0]
-        else:
-            break
-    return shape
-
 def cat_matrices2D(mat1, mat2, axis=0):
-    '''function2 documented'''
+    '''function documented'''
     result = mat1[::]
     if axis == 0:
         result += mat2
+        if len(result) > 0 and len(mat2) > 0:
+            if len(mat1[0]) != len(mat2[0]):
+                return None
     else:
-        for i in range(min(len(mat1), len(mat2))):
+        if len(mat1) != len(mat2):
+            return None
+        for i in range(min(len(result), len(mat2))):
             result[i] += mat2[i]
     return result
