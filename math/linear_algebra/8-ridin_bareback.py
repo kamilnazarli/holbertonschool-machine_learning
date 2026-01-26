7e28ab200de9cdba4ce0d69394e3a832b7258c94
Kamil <nezerovk425@gmail.com>
2026-01-26 11:50:38 +0000

    Matrix multiplication

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''module documented'''
+
+
+def mat_mul(mat1, mat2):
+    '''function documented'''
+    res = []
+    for i in range(len(mat1)):
+        new_row = []
+        for j in range(len(mat2[0])):
+            temp = [] #keeps multiples for sum
+            for k in range(len(mat1[0])):
+                temp.append(mat1[i][k]*mat2[k][j])
+            new_row.append(sum(temp))
+        res.append(new_row)
+    return res
