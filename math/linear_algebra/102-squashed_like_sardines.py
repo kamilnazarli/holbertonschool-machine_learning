b11b6b15d5bb6cd629cefc0ce664afaf59a72350
Kamil <nezerovk425@gmail.com>
2026-01-28 11:31:25 +0000

    Concatenate matrices

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -2,8 +2,29 @@
 '''module documented'''
 
 
+def matrix_shape(matrix):
+    '''function1 documented'''
+    shape = []
+    current = matrix
+    while True:
+        if type(current) is list and len(current) > 0:
+            shape.append(len(current))
+            current = current[0]
+        else:
+            break
+    return shape
+
+def cat_mat1D(mat1, mat2):
+    '''function1 documented'''
+    result = mat1[::]
+    for i in mat2:
+        result.append(i)
+    return result
+
 def cat_matrices(mat1, mat2, axis=0):
-    '''function documented'''
+    '''function2 documented'''
+    if len(matrix_shape(mat1)) == 1 and len(matrix_shape(mat2)) == 1:
+        return cat_mat1D(mat1, mat2)
     result = mat1[::]
     if axis == 0:
         result += mat2
