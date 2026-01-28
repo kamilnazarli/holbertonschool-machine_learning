7d51642c8ac970b91f12569a0a353892adc0812a
Kamil <nezerovk425@gmail.com>
2026-01-28 11:02:54 +0000

    Add matrices

---
+++ math/linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''module documented'''
+
+
+def matrix_shape(matrix):
+    '''function1 documented'''
+    shape = []
+    current = matrix
+    while True:
+        if type(current) is list and len(current) > 0:
+            shape.append(len(current))
+            current = current[0]
+        else:
+            break
+    return shape
+
+
+def add_matrices(arr1, arr2):
+    '''function2 documented'''
+    if len(arr1) == 0 and len(arr2) == 0:
+        return []
+    shape1 = matrix_shape(arr1)
+    shape2 = matrix_shape(arr2)
+    if shape1 != shape2:
+        return None
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i]+arr2[i])
+    return result
