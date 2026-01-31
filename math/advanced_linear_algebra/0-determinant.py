2472186397794086bedc1956b044d813d470b666
Kamil <nezerovk425@gmail.com>
2026-01-31 18:06:22 +0400

    determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -25,7 +25,7 @@ def determinant(matrix):
         raise TypeError("matrix must be a list of lists")
     
     if not check_shape(matrix):
-        raise ValueError("matrix must be square matrix")
+        raise ValueError("matrix must be a square matrix")
      
     A = [row[:] for row in matrix]
     rows = len(A)
