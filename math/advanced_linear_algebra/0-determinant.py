faad8df1d5bfd07e0df96658c6c7b1775f05b1d1
Kamil <nezerovk425@gmail.com>
2026-01-31 17:45:45 +0400

    find determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
 '''module documented'''
+def check_shape(matrix):
+    '''function1 documented'''
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return True
+    return len(matrix)==len(matrix[0])
 
+def check(ls):
+    '''function2 documented'''
+    if type(ls) is not list:
+        return False
+
+    if len(ls) == 0:
+        return False
+
+    for row in ls:
+        if type(row) is not list:
+            return False
+    return True
 
 def determinant(matrix):
-    '''function documented''' 
+    '''function3 documented'''
+    if not check_shape(matrix):
+        raise ValueError("matrix must be square matrix")
+    
+    if not check(matrix):
+        raise TypeError("matrix must be a list of lists")
+    
     A = [row[:] for row in matrix]
     rows = len(A)
     cols = len(A[0])
@@ -23,4 +46,11 @@ def determinant(matrix):
             for c in range(cols):
                 A[r][c] = A[r][c] - factor * A[pivot_row][c]
         pivot_row += 1
-    return A
+
+    det = 1
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            if i == j:
+                det *= A[i][j]
+    
+    return det
