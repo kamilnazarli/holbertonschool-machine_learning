c6368f7e4719a8a96bd72a551e05934d51252326
Kamil <nezerovk425@gmail.com>
2026-02-01 15:47:36 +0400

    adjugate matrix

---
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+Determinant of a matrix
+"""
+
+
+def matrix_transpose(matrix):
+    '''transpose function'''
+    new_matrix = []
+    for col in range(len(matrix[0])):
+        temp = []
+        for row in range(len(matrix)):
+            temp.append(matrix[row][col])
+        new_matrix.append(temp)
+    return new_matrix
+
+
+def determinant(mat):
+    """
+    Calculation of determinant of a matrix
+    """
+    x = len(mat)
+
+    if x == 1:
+        return mat[0][0]
+    if x == 2:
+        return mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1]
+
+    return sum(
+        (-1) ** k * mat[0][k] *
+        determinant([row[:k] + row[k + 1:] for row in mat[1:]])
+        for k in range(x)
+    )
+
+
+def minor(matrix):
+    '''Minor function'''
+    if (not isinstance(matrix, list) or
+            any(not isinstance(row, list) for row in matrix)):
+        raise TypeError("matrix must be a list of lists")
+    x = len(matrix)
+    if any(len(row) != x for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+    if x == 1:
+        return [[1]]
+    minor = [[0 for _ in range(x)] for _ in range(x)]
+    for i in range(x):
+        for j in range(x):
+            sub_matrix = [
+                [matrix[m][n] for n in range(x) if n != j]
+                for m in range(x) if m != i]
+            minor[i][j] = determinant(sub_matrix)
+    return minor
+
+
+def cofactor(matrix):
+    '''cofactor function'''
+    minor_matrix = minor(matrix)
+    n = len(minor_matrix)
+    for i in range(n):
+        for j in range(n):
+            minor_matrix[i][j] = (-1) ** (i + j) * minor_matrix[i][j]
+    
+    return minor_matrix
+
+def adjugate(matrix):
+    '''adjugate matrix-
+    transposed of cofactor matrix
+    '''
+    cofactor_matrix = cofactor(matrix)
+    return matrix_transpose(cofactor_matrix)
