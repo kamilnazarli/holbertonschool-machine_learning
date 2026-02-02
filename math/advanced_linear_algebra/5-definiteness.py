cccab64390ee8d89b9b88639fd53dd52c220f3cb
Kamil <nezerovk425@gmail.com>
2026-02-02 13:07:14 +0400

    find definiteness

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -3,12 +3,25 @@ import numpy as np
 '''module documented'''
 
 
+def matrix_transpose(matrix):
+    '''transpose function'''
+    new_matrix = []
+    for col in range(len(matrix[0])):
+        temp = []
+        for row in range(len(matrix)):
+            temp.append(matrix[row][col])
+        new_matrix.append(temp)
+    return new_matrix
+
+
 def definiteness(matrix):
     '''function documented'''
     if type(matrix) != np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")
     x = len(matrix)
     if any(len(row) != x for row in matrix):
+        return None
+    if matrix_transpose(matrix) != matrix:
         return None
     eigen_values = np.linalg.eigvalsh(matrix)
     if all(eigen_values > 0):
