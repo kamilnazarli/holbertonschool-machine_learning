e751db4db656e8b5088fb1f1ac20f1191a7c527e
Kamil <nezerovk425@gmail.com>
2026-02-04 16:00:50 +0400

    added pmf calculation

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -17,3 +17,18 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
+    def pmf(self, k):
+        '''method1 documented'''
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        p = (self.lambtha ** k) * 2.7182818285 ** (-self.lambtha) / Poisson.factorial(k)
+        return p
+    @staticmethod
+    def factorial(n):
+        '''method2 documented'''
+        p = 1
+        for i in range(1, n+1):
+            p *= i
+        return p
