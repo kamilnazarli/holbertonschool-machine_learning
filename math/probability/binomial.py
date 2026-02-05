38363ceaef95207121434ae91ea85ddd481727f3
Kamil <nezerovk425@gmail.com>
2026-02-05 15:17:44 +0400

    pmf

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -34,7 +34,7 @@ class Binomial:
             return 0
         return ((self.factorial(self.n) /
                  (self.factorial(k) * self.factorial(self.n - k))) *
-                 (self.p ** k) * (1 - self.p) ** (self.n - k))
+                       (self.p ** k) * (1 - self.p) ** (self.n - k))
 
     @staticmethod
     def factorial(n):
