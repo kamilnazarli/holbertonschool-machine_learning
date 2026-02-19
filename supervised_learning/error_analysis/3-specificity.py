b5f4b052164e8b53ef112d485ecc9dcec8f442fd
Kamil <nezerovk425@gmail.com>
2026-02-19 11:28:53 +0400

    specificity

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def specificity(confusion):
+    '''function documented'''
+    res = []
+    for i in range(len(confusion)):
+        TN, FP, spec = 0, 0, 0
+        id = 0
+        for j in range(len(confusion[i])):
+            if i != id and j != id:
+                TN = confusion[j][i]
+            else:
+                FP += confusion[j][i]
+        spec = TN / (TN + FP)
+        res.append(spec)
+    return np.asarray(res)
