e39b665e6604db4a594306c6671965920a567c02
Kamil <nezerovk425@gmail.com>
2026-02-21 17:54:26 +0400

    pycodestyle regulation

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-
+'''module documented'''
 import numpy as np
 
+
 class Node:
+    '''Node class documented'''
     def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+        '''init documented'''
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -14,6 +17,7 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
+        '''method documented'''
         if (self.right_child.max_depth_below() >
             self.left_child.max_depth_below()):
                 max_d = self.right_child
@@ -22,17 +26,22 @@ class Node:
         return max_d.max_depth_below()
 
 class Leaf(Node):
+    '''Leaf class documented'''
     def __init__(self, value, depth=None):
+        '''init documented'''
         super().__init__()
         self.value = value
         self.is_leaf = True
         self.depth = depth
 
     def max_depth_below(self):
+        '''method documented'''
         return self.depth
 
 class Decision_Tree():
+    '''decision_tree class documented'''
     def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+        '''init documented'''
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -45,5 +54,6 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
+        '''method documented'''
         return self.root.max_depth_below()
