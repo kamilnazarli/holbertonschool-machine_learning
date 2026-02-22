d9b658090f5c53b46331fd34cd6af2bc9537b36a
Kamil <nezerovk425@gmail.com>
2026-02-22 15:57:50 +0400

    try

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -23,28 +23,28 @@ class Node:
             return f"-> leaf [value={self.value}]"
 
         if self.is_root:
-            node_line = f"root [feature={self.feature}, threshold={self.threshold}]"
+            text = f"root [feature={self.feature}, threshold={self.threshold}]\n"
         else:
-            node_line = f"-> node [feature={self.feature}, threshold={self.threshold}]"
+            text = f"-> node [feature={self.feature}, threshold={self.threshold}]\n"
 
-        left_text = self.left_child_add_prefix(self.left_child.__str__()) if self.left_child else ""
-        right_text = self.right_child_add_prefix(self.right_child.__str__()) if self.right_child else ""
+        text += self.left_child_add_prefix(self.left_child.__str__()) if self.left_child else ""
+        text += self.right_child_add_prefix(self.right_child.__str__()) if self.right_child else ""
 
-        return node_line + "\n" + left_text + right_text
+        return text.rstrip("\n")
 
     def left_child_add_prefix(self, text):
         lines = text.split("\n")
         new_text = "+--->" + lines[0] + "\n"
         for line in lines[1:]:
             new_text += "|   " + line + "\n"
-        return new_text.rstrip("\n")
+        return new_text
 
     def right_child_add_prefix(self, text):
         lines = text.split("\n")
         new_text = "+--->" + lines[0] + "\n"
         for line in lines[1:]:
             new_text += "    " + line + "\n"
-        return new_text.rstrip("\n")
+        return new_text
     
     def max_depth_below(self):
         '''method documented'''
