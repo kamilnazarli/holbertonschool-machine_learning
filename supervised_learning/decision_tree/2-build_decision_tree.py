00d09b17740ff7a6ded2021f51bd59dc66ee76ed
Kamil <nezerovk425@gmail.com>
2026-02-22 15:45:58 +0400

    try

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -17,23 +17,22 @@ class Node:
         self.sub_population = None
         self.depth = depth
     
-    def __str__(self) :
+    def __str__(self):
         '''__str__ method'''
         if self.is_leaf:
-            return (f"-> leaf [value={self.value}]")
+            return f"-> leaf [value={self.value}]"
+    
+        # root or internal node line
         if self.is_root:
-            node_line = (f"root [feature={self.feature}, threshold={self.threshold}]")
+            node_line = f"root [feature={self.feature}, threshold={self.threshold}]"
         else:
-            node_line = (f"-> node [feature={self.feature}, threshold={self.threshold}]")
-        left_text = ""
-        right_text = ""
-        if self.left_child:
-            # temp = self.left_child.__str__()
-            left_text += self.left_child_add_prefix(self.left_child.__str__())
-        if self.right_child:
-            # temp = self.right_child.__str__()
-            right_text += self.right_child_add_prefix(self.right_child.__str__())
-        return node_line + '\n' + left_text + right_text
+            node_line = f"-> node [feature={self.feature}, threshold={self.threshold}]"
+    
+        # recursively get children strings
+        left_text = self.left_child_add_prefix(self.left_child.__str__()) if self.left_child else ""
+        right_text = self.right_child_add_prefix(self.right_child.__str__()) if self.right_child else ""
+    
+        return node_line + "\n" + left_text + right_text
 
     def left_child_add_prefix(self,text):
             lines=text.split("\n")
