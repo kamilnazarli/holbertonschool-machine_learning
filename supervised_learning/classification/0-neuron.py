68b3403415caa875578ff2192bf684476044f9ac
Kamil <nezerovk425@gmail.com>
2026-03-03 12:03:48 +0400

    neuron class

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -10,6 +10,6 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        w = np.random.normal(loc=1, scale=3, size=10)
+        W = np.random.normal(loc=1, scale=3, size=10)
         b = 0
         A = 0
