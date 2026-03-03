adddaed86c70e3377afb4545a68361c9a91f07cd
Kamil <nezerovk425@gmail.com>
2026-03-03 20:01:14 +0400

    nn

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+class Neuron:
+    '''class documented'''
+
+    def __init__(self, nx):
+        '''init documented'''
+        if not (isinstance(nx, int)):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+    @property
+    def W(self):
+        '''getter for W'''
+        return self.__W
+    @property
+    def b(self):
+        '''getter for b'''
+        return self.__b
+    @property
+    def A(self):
+        '''getter for A'''
+        return self.__A
