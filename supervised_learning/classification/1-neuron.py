192fc0b8ef6ebee55b392a92d656f07c1a08345f
Kamil <nezerovk425@gmail.com>
2026-03-03 20:02:07 +0400

    nn

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -15,14 +15,17 @@ class Neuron:
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
+
     @property
     def W(self):
         '''getter for W'''
         return self.__W
+
     @property
     def b(self):
         '''getter for b'''
         return self.__b
+
     @property
     def A(self):
         '''getter for A'''
