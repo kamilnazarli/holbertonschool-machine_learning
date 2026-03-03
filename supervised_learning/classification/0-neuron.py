5f6825e4c98e2457b1f79ec26829819c30fa8697
Kamil <nezerovk425@gmail.com>
2026-03-03 12:05:04 +0400

    neuron class

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -5,11 +5,11 @@ import numpy as np
 
 class Neuron:
     '''class documented'''
+    W = np.random.normal(loc=1, scale=3, size=10)
+    b = 0
+    A = 0
     def __init__(self, nx):
         if not(isinstance(nx, int)):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        W = np.random.normal(loc=1, scale=3, size=10)
-        b = 0
-        A = 0
