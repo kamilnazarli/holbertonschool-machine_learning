9cde40746393e4f705a300b93cb13a60f68db653
Kamil <nezerovk425@gmail.com>
2026-03-03 20:00:10 +0400

    neuron class

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,6 +12,18 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+    @property
+    def W(self):
+        '''getter for W'''
+        return self.__W
+    @property
+    def b(self):
+        '''getter for b'''
+        return self.__b
+    @property
+    def A(self):
+        '''getter for A'''
+        return self.__A
