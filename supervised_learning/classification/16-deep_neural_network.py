310a06e5fa47b63a03094006a53cce85d21bbe52
Kamil <nezerovk425@gmail.com>
2026-03-08 13:23:46 +0400

    nn

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -22,9 +22,11 @@ class DeepNeuralNetwork:
         np.random.seed(0)
         for l in range(1, self.L):
             W = (np.random.randn(layers[l], layers[l-1]) *
-                      np.sqrt(2/layers[l-1]))
+                      np.sqrt(2 / layers[l-1]))
             b = np.zeros((layers[l], 1))
-            self.weights.update({f"W{l}": W})
-            self.weights.update({f"b{l}": b})
+            # self.weights.update({f"W{l}": W})
+            # self.weights.update({f"b{l}": b})
+            self.weights[f"W{l}"] = W
+            self.weights[f"b{l}"] = b
             # W1 : W  randn(3, 5)
             # b1 : b zeros()
