550ed1da7de2f14f95c9751ed680c6e391d7cd8f
Kamil <nezerovk425@gmail.com>
2026-03-08 18:10:00 +0400

    final

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,9 +5,9 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     res = np.zeros((classes, len(Y)))
-    for i in range(len(Y)):
-        temp = np.zeros((1, classes))
-        temp[0, i] = 1
+    for i in range(classes):
+        temp = np.zeros((1, len(Y)))
+        temp[0, Y[i]] = 1
         res[i] = temp
     return res.T
 
