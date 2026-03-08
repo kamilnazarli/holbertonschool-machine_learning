3fb9bb9e201d49111b2d2f37987d4a638ae2ee5b
Kamil <nezerovk425@gmail.com>
2026-03-08 21:33:34 +0400

    try

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -6,7 +6,7 @@ import numpy as np
 def one_hot_decode(one_hot):
     '''one hot decoding'''
     res = one_hot.T
-    temp = np.zeros((one_hot.shape[1],), dtype=int)
+    temp = np.zeros((one_hot.shape[1], 1), dtype=int)
     for i in range(one_hot.shape[1]):
         temp[i] = np.argmax(res[i])
     return temp
