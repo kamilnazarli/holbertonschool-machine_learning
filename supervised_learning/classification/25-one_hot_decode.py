4d14da553de0797c7196a860336028ae4009404d
Kamil <nezerovk425@gmail.com>
2026-03-08 21:31:20 +0400

    decoding

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     '''one hot decoding'''
+    res = one_hot.T
     temp = np.zeros((one_hot.shape[1],), dtype=int)
-    for i in range(one_hot.shape[0]):
-        temp[i] = np.argmax(one_hot[i])
+    for i in range(one_hot.shape[1]):
+        temp[i] = np.argmax(res[i])
     return temp
