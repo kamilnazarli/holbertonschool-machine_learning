832c82499f1db797bbdab4703c3fd839a3b30da6
Kamil <nezerovk425@gmail.com>
2026-03-08 21:02:26 +0400

    one hot

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -9,6 +9,10 @@ def one_hot_encode(Y, classes):
         return None
     if not (isinstance(classes, int)):
         return None
+    if classes < 2:
+        return None
+    if classes < np.max(Y):
+        return None
     res = np.zeros((len(Y), classes))
     for i in range(len(Y)):
         temp = np.zeros((1, classes))
