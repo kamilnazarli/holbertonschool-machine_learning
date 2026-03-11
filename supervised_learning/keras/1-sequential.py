ad192566f6314dc5140162123acdfde7b76787b2
Kamil <nezerovk425@gmail.com>
2026-03-11 21:16:43 +0400

    model without Sequential

---
+++ supervised_learning/keras/1-sequential.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''module documented'''
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''function documented'''
+    # 784, [256, 256, 10], ['tanh', 'tanh', 'softmax'], 0.001, 0.95
+    inputs = K.Input(shape=(nx,))
+    x = inputs
+    for i in range(len(layers)):
+        x = K.layers.Dense(
+            layers[i], 
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+        )(x)
+
+        if i != len(layers) - 1:
+            x = K.layers.Dropout(rate=1-keep_prob)(x)
+    model = K.Model(inputs=inputs, outputs=x)
+    return model
