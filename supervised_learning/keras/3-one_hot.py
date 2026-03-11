4010148d95fa6cc7a8d8dd1326ca34019cf171b7
Kamil <nezerovk425@gmail.com>
2026-03-11 22:00:48 +0400

    one hot

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -5,6 +5,8 @@ import tensorflow.keras as K
 
 def one_hot(labels, classes=None):
     '''function documented'''
+    if classes is None:
+        classes = max(labels) + 1
     layer = K.layers.CategoryEncoding(
         num_tokens=classes,
         output_mode="one_hot"
