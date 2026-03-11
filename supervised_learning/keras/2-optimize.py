65b51f6d7210823fd5527f6dda0fda7b82c963b7
Kamil <nezerovk425@gmail.com>
2026-03-11 21:39:58 +0400

    optimized

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -10,7 +10,7 @@ def optimize_model(network, alpha, beta1, beta2):
         beta_1=beta1,
         beta_2=beta2
         )
-    network.compile(loss="categorical_crossentropy", 
+    network.compile(loss="categorical_crossentropy",
                     optimizer=opt
                     )
     return None
