b705a55eb93270fd08c8f2910495557980a5552a
Kamil <nezerovk425@gmail.com>
2026-03-12 21:31:47 +0400

    fix: deleted redundant whitespaces

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -3,21 +3,14 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
     '''
-    Args: 
-        network is the model to train
-        data is a numpy.ndarray of shape (m, nx) containing the input data
-        labels is a one-hot numpy.ndarray of shape (m, classes) containing the labels of data
-        batch_size is the size of the batch used for mini-batch gradient descent
-        epochs is the number of passes through data for mini-batch gradient descent
-        verbose is a boolean that determines if output should be printed during training
-        shuffle is a boolean that determines whether to shuffle the batches every epoch
-        validation_data is the data to validate the model with
+    function documented
     '''
     if validation_data is not None:
         return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
-                           verbose=verbose, shuffle=shuffle, 
+                           verbose=verbose, shuffle=shuffle,
                            validation_data=validation_data)
     else:
         return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs, 
