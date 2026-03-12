81550ac2205bae4bdd55c96e27c079b68f4b8955
Kamil <nezerovk425@gmail.com>
2026-03-12 21:43:01 +0400

    early stopping added to training

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''module documented'''
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, 
+                patience=0, verbose=True, shuffle=False):
+    '''
+    function documented
+    '''
+    if validation_data is not None:
+        early_stopping = K.callbacks.EarlyStopping(monitor="val_loss",
+                                                   patience=patience)
+        return network.fit(x=data, y=labels,
+                           batch_size=batch_size, epochs=epochs,
+                           callbacks=[early_stopping],
+                           verbose=verbose, shuffle=shuffle,
+                           validation_data=validation_data)
+    else:
+        return network.fit(x=data, y=labels, batch_size=batch_size,
+                           epochs=epochs,
+                           verbose=verbose, shuffle=shuffle)
