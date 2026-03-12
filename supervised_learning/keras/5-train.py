b3a2618dae301f45c1d7254bee47e127c858635d
Kamil <nezerovk425@gmail.com>
2026-03-12 21:33:22 +0400

    delete whitespace

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -15,5 +15,5 @@ def train_model(network, data, labels, batch_size, epochs,
                            validation_data=validation_data)
     else:
         return network.fit(x=data, y=labels, batch_size=batch_size,
-                           epochs=epochs, 
+                           epochs=epochs,
                            verbose=verbose, shuffle=shuffle)
