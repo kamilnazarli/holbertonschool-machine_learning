1ccbb4e6cbbf9cb4b97585b6d6693c147335ca3d
Kamil <nezerovk425@gmail.com>
2026-03-12 20:13:51 +0400

    training model

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''module documented'''
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    '''funcion documented'''
+    history = network.fit(data,
+                          labels,
+                          epochs=epochs,
+                          batch_size=batch_size,
+                          verbose=verbose,
+                          shuffle=shuffle
+                          )
+    return history
