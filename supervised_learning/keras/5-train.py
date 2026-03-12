6bab9db282cbd0e3ddf247e4609eb3627de10a29
Kamil <nezerovk425@gmail.com>
2026-03-12 20:55:46 +0400

    fix: checking whether validation data is none

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -15,6 +15,10 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
         shuffle is a boolean that determines whether to shuffle the batches every epoch
         validation_data is the data to validate the model with
     '''
-    return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
-                       verbose=verbose, shuffle=shuffle, 
-                       validation_data=validation_data)
+    if validation_data is None:
+        return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
+                           verbose=verbose, shuffle=shuffle, 
+                           validation_data=validation_data)
+    else:
+        return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
+                   verbose=verbose, shuffle=shuffle)
