e85881ea47550720a1d9d7615cc7ad3700e95500
Kamil <nezerovk425@gmail.com>
2026-03-12 21:51:09 +0400

    deleted whitespace

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -4,8 +4,9 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, early_stopping=False,
-                patience=0, verbose=True, shuffle=False):
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
     '''
     function documented
     '''
