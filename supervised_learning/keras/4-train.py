b90116acade5ada67502790867463607ea32180d
Kamil <nezerovk425@gmail.com>
2026-03-12 20:44:38 +0400

    training a model using keras

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -5,9 +5,10 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
     '''funcion documented'''
-    history = network.fit(x=data,
-                          y=labels,
-                          batch_size=batch_size,
-                          epochs=epochs
-                          )
-    return history
+    return network.fit(x=data,
+                       y=labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle
+                       )
