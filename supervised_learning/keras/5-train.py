347bd2fccf430143d4039f4ba67cdc9051afaef1
Kamil <nezerovk425@gmail.com>
2026-03-12 21:32:13 +0400

    fix: deleting redundant whitespaces

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -9,9 +9,11 @@ def train_model(network, data, labels, batch_size, epochs,
     function documented
     '''
     if validation_data is not None:
-        return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs,
+        return network.fit(x=data, y=labels, batch_size=batch_size,
+                           epochs=epochs,
                            verbose=verbose, shuffle=shuffle,
                            validation_data=validation_data)
     else:
-        return network.fit(x=data, y=labels, batch_size=batch_size, epochs=epochs, 
+        return network.fit(x=data, y=labels, batch_size=batch_size,
+                           epochs=epochs, 
                            verbose=verbose, shuffle=shuffle)
