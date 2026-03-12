7c003fbea897fcfadd9785937aca727014041635
Kamil <nezerovk425@gmail.com>
2026-03-12 20:36:11 +0400

    training

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -8,8 +8,6 @@ def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle
     history = network.fit(data,
                           labels,
                           epochs=epochs,
-                          batch_size=batch_size,
-                          verbose=verbose,
-                          shuffle=shuffle
+                          batch_size=batch_size
                           )
     return history
