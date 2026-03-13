0baae601342882945959b44c74540b6698a4c17f
Kamil <nezerovk425@gmail.com>
2026-03-13 20:03:00 +0400

    saving and loading model

---
+++ supervised_learning/keras/9-train.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''module documented'''
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    '''saver'''
+    network.save(filename)
+    return None
+
+def load_model(filename):
+    '''loader'''
+    return K.saving.load_model(filename)
