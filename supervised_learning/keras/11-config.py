7d51932036dd9be544ef42520d7bb041a9eaf17e
Kamil <nezerovk425@gmail.com>
2026-03-13 20:17:48 +0400

    saving and loading config

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -5,10 +5,14 @@ import tensorflow.keras as K
 
 def save_config(network, filename):
     '''saver'''
-    network.get_config(filename)
+    json_string = network.to_json()
+    with open(filename, "w") as f:
+        f.write(json_string)
     return None
 
 
 def load_config(filename):
     '''loader'''
-    return K.models.model_from_json(filename)
+    with open(filename, "r") as f:
+        json_config = f.read()
+    return K.models.model_from_json(json_config)
