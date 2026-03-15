0c751c103499121f84f5769ae3a2e02ddda91682
Kamil <nezerovk425@gmail.com>
2026-03-15 19:08:23 +0400

    function inside changed

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -4,8 +4,11 @@ import numpy as np
 
 
 def moving_average(data, beta):
-    """method"""
-    ewa = [data[0]]
-    for i in range(1, len(data)):
-        ewa.append(beta * ewa[i - 1] + (1 - beta) * data[i])
+    ewa = []
+    temp = 0
+    for i in range(1, len(data)+1):
+        temp = beta * temp + (1 - beta) * data[i-1]
+        temp_c= temp / (1 - beta ** i)
+        ewa.append(temp_c)
     return ewa
+
