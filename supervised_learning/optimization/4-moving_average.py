c91b66862e9fa63769544b24e7034c9f16b0c601
Kamil <nezerovk425@gmail.com>
2026-03-15 19:10:41 +0400

    fixed whitespace around operator

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -8,7 +8,7 @@ def moving_average(data, beta):
     ewa = []
     temp = 0
     for i in range(1, len(data) + 1):
-        temp = beta * temp + (1 - beta) * data[i-1]
+        temp = beta * temp + (1 - beta) * data[i - 1]
         temp_c= temp / (1 - beta ** i)
         ewa.append(temp_c)
     return ewa
