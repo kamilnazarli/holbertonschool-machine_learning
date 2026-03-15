8b88394b5c4a0870bb23aa72e0c63af377869221
Kamil <nezerovk425@gmail.com>
2026-03-15 23:49:32 +0400

    added underscores

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -7,7 +7,7 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
     '''Adam in keras'''
     return tf.keras.optimizers.Adam(
         learning_rate=alpha,
-        beta1=beta1,
-        beta2=beta2,
+        beta_1=beta1,
+        beta_2=beta2,
         epsilon=epsilon
     )
