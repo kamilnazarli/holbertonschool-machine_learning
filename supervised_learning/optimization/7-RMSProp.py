66837aa9c41869ffdd0ec649f0a06b4c8797190a
Kamil <nezerovk425@gmail.com>
2026-03-15 22:50:10 +0400

    updating variables with rmsprop

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    '''gradient descent with momentum'''
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
+    var = var - alpha * (grad / np.sqrt(s + epsilon))
+    return var, s
