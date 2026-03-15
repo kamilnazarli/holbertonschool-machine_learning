83f0576b0c8d1ec7633b9cf66485dc41dfcf9737
Kamil <nezerovk425@gmail.com>
2026-03-15 22:53:44 +0400

    fixed sqrt part

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -6,5 +6,5 @@ import numpy as np
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     '''gradient descent with momentum'''
     s = beta2 * s + (1 - beta2) * (grad ** 2)
-    var = var - alpha * (grad / (s + epsilon) ** 0.5)
+    var = var - alpha * (grad / (s ** 0.5 + epsilon))
     return var, s
