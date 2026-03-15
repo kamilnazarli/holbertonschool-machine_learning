ab273cb07294390ff11b37056ac1f30fae29aa46
Kamil <nezerovk425@gmail.com>
2026-03-15 14:58:13 +0400

    fixed

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 '''module documented'''
-shuffle_data = __import__("2-shuffle_data").shuffle_data
-
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 def create_mini_batches(X, Y, batch_size):
     '''method'''
