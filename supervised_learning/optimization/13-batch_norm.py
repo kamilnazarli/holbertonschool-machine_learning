ae6d03f7823a831720e68b01d31196b0ccc1f639
Kamil <nezerovk425@gmail.com>
2026-03-16 23:59:19 +0400

    batch normalization

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    '''Batch Normalization'''
+    mean = np.mean(Z, axis=1)
+    std= np.std(Z, axis=1)
+    return (Z - mean) / (np.sqrt(std ** 2) + epsilon)
