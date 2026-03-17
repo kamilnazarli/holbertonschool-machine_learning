f9b208eae05228024d40119664da0ff48822f07b
Kamil <nezerovk425@gmail.com>
2026-03-17 10:52:27 +0400

    added beta and gamma externally

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 
 def create_batch_norm_layer(prev, n, activation):
     '''batch normalization'''
-    gamma = [1]
-    beta = [0]
+    beta = tf.keras.initializers.Zeros()
+    gamma = tf.keras.initializers.Ones()
     initializer = tf.keras.initializers.VarianceScaling(
         mode="fan_avg"
         )
@@ -14,8 +14,8 @@ def create_batch_norm_layer(prev, n, activation):
                               kernel_initializer=initializer
                               )(prev)
     bn = tf.keras.layers.BatchNormalization(epsilon=1e-7,
-                                            beta_initializer="zeros",
-                                            gamma_initializer="ones")
+                                            beta_initializer=beta,
+                                            gamma_initializer=gamma)
     Z_norm = bn(Z)
     activated = activation(Z_norm)
     return activated
