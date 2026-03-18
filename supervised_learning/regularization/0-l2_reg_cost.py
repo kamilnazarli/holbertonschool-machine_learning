03171e5ca865a5e0f154ad0011e46522b5695523
Kamil <nezerovk425@gmail.com>
2026-03-18 19:11:54 +0400

    variable name changed

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -6,7 +6,7 @@ import numpy as np
 def l2_reg_cost(cost, lambtha, weights, L, m):
     '''l2 regularization'''
     s = 0
-    for l in range(1, L + 1):
-        s += np.sum(weights[f"W{l}"] ** 2)
+    for i in range(1, L + 1):
+        s += np.sum(weights[f"W{i}"] ** 2)
     cost += s * lambtha / (2 * m)
     return cost
