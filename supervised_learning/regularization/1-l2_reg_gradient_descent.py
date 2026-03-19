34e4bf885c33beb9c3c358215f7bd59a632466bc
Kamil <you@example.com>
2026-03-19 16:26:48 +0400

    l2 reg

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -7,21 +7,18 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lamtha, L):
     '''l2 gradient descent'''
     m = Y.shape[1]  # number of data points
     k = Y.shape[0]  # classes
-    for i in range(1, L + 1):
+    dz = cache[f"A{L}"]
+    for i in range(L, 0, -1):
         W = weights[f"W{i}"]
         A_prev = cache[f"A{i - 1}"]
-        bias = weights[f"b{i}"]
-        Z = np.matmul(W, A_prev) + bias
-        if i != L:
-            A = np.tanh(Z)
-        else:
-            # softmax implementation
-            z_shifted = Z - np.max(Z, axis=0, keepdims=True)
-            A = (np.exp(z_shifted) /
-                 np.sum(np.exp(z_shifted), axis=0, keepdims=True))
-        dZ = A - Y
-        dW = (1 / m) * (np.matmul(dZ, A_prev) + lamtha * W)
+
+        dW = (1 / m) * (np.matmul(dZ, A_prev.T) + lamtha * W)
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        bias = weights[f"b{i}"]
+        if i > 1:
+            A_prev = cache[f"A{i - 1}"]
+            dZ = np.matmul(W.T, dZ) * (1 - A_prev**2)
 
         weights[f"W{i}"] = weights[f"W{i}"] - alpha * dW
         weights[f"b{i}"] = weights[f"b{i}"] - alpha * db
