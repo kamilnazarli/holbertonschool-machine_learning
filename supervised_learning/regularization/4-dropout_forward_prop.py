e4c26051e11a0c6080c8f9212a9060115c37eb0c
Kamil <nezerovk425@gmail.com>
2026-03-20 23:12:17 +0400

    added one blank line

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -8,6 +8,7 @@ def softmax(x):
     e_x = np.exp(x - np.max(x, axis=0, keepdims=True))
     return e_x / np.sum(e_x, axis=0, keepdims=True)
 
+
 def dropout_forward_prop(X, weights, L, keep_prob):
     '''forward prop with dropout'''
     outputs = {"A0": X}
