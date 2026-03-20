a2121ba029c4cbbfad9bbcacd090402d2c39ac4a
Kamil <nezerovk425@gmail.com>
2026-03-20 14:43:06 +0400

    creating new layer with l2 reg

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''module documented'''
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    '''creating nn layer using L2'''
+    layer = tf.keras.layers.Dense(
+        n,
+        activation=activation,
+        kernel_regularizer=tf.keras.regularizers.l2(lambtha)
+    )
+    return layer(prev)
