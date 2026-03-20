0da90c0012c9c9a81832efc5a17928beb1bdd957
Kamil <nezerovk425@gmail.com>
2026-03-20 11:14:51 +0400

    l2 regularization + gradient descent

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -5,20 +5,16 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lamtha, L):
     '''l2 gradient descent'''
-    m = Y.shape[1]  # number of data points
-    k = Y.shape[0]  # classes
-    dZ = cache[f"A{L}"]
+    m = Y.shape[1]
+    dZ = cache[f"A{L}"] - Y
     for i in range(L, 0, -1):
         W = weights[f"W{i}"]
         A_prev = cache[f"A{i - 1}"]
 
-        dW = (1 / m) * (np.matmul(dZ, A_prev.T) + lamtha * W)
+        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lamtha / m) * W
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        bias = weights[f"b{i}"]
         if i > 1:
-            A_prev = cache[f"A{i - 1}"]
-            dZ = np.matmul(W.T, dZ) * (1 - A_prev**2)
-
-        weights[f"W{i}"] = weights[f"W{i}"] - alpha * dW
-        weights[f"b{i}"] = weights[f"b{i}"] - alpha * db
+            dZ = np.matmul(W.T, dZ) * (1 - A_prev**2)  # tanh
+        weights[f"W{i}"] -= alpha * dW
+        weights[f"b{i}"] -= alpha * db
