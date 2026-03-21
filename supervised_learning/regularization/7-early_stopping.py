ab8b388bedaa312a79a019451cb01e066729d576
Kamil <nezerovk425@gmail.com>
2026-03-21 12:29:25 +0400

    try

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """early stopping"""
+    while count != patience:
+        if cost + threshold > opt_cost:
+            count += 1
+        else:
+            opt_cost = cost + threshold
+            count = 0
+    if count == patience:
+        return False, count
+    else:
+        return True, count
