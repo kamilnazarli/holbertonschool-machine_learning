f438c2c8e7ae211be6bc067a79b12d7ed72a0214
Kamil <nezerovk425@gmail.com>
2026-03-21 12:49:09 +0400

    changed true and false

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -12,6 +12,6 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
         count = 0
 
     if count >= patience:
-        return False, count
-    else:
         return True, count
+    else:
+        return False, count
