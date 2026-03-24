cb93794ff6da6a200eac5fda9609f18e06fd8548
Kamil <nezerovk425@gmail.com>
2026-03-24 23:40:26 +0400

    image convolution

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    '''convolving images'''
+    h, kh = images.shape[1], kernel.shape[0]
+    w, kw = images.shape[2], kernel.shape[1]
+    oh, ow = (h - kh + 1), (w - kw + 1)
+    output = np.zeros(shape=(images.shape[0], oh, ow))
+    
+    for row in range(h):
+        for col in range(w):
+            patch = images[:, row:row+2, col:col+2]
+            output[:, row:row+2, col:col+2] = np.sum(patch * kernel)
+    return output
