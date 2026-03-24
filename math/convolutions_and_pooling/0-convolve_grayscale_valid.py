c02fc8088f5c0f0817c39bfe447fad3e992901dd
Kamil <nezerovk425@gmail.com>
2026-03-24 23:44:58 +0400

    width and height fixed

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -12,6 +12,6 @@ def convolve_grayscale_valid(images, kernel):
     
     for row in range(h):
         for col in range(w):
-            patch = images[:, row:row+2, col:col+2]
+            patch = images[:, row:row+kh, col:col+kw]
             output[:, row, col] = np.sum(patch * kernel)
     return output
