4e379b102a87de32eed76015fd9c2bdfeba058e2
Kamil <nezerovk425@gmail.com>
2026-03-26 23:26:22 +0400

    convolving an image with strides

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    '''same convolution'''
+    h, kh = images.shape[1], kernel.shape[0]
+    w, kw = images.shape[2], kernel.shape[1]
+    sh, sw = stride
+    if padding == "same":
+        if kh % 2 != 0:
+            ph = (kh - 1) // 2
+        else:
+            ph = int(np.ceil((kh - 1) / 2))
+        if kw % 2 != 0:
+            pw = (kw - 1) // 2
+        else:
+            pw = int(np.ceil((kw - 1) / 2))
+        oh, ow = h, w
+
+    elif padding == "valid":
+        oh, ow = (int(np.ceil((h - kh + 1) / sh)),
+                  int(np.ceil((w - kw + 1) / sw)))
+
+    else:
+        ph, pw = padding
+        oh, ow = (((h - kh + 2 * ph) / sh + 1),
+              ((w - kw + 2 * pw) / sw + 1))
+
+        images = np.pad(images,
+                        ((0, 0), (ph, ph), (pw, pw)),
+                        constant_values=(0))
+    output = np.zeros(shape=(images.shape[0], oh, ow))
+    for row in range(oh):
+        for col in range(ow):
+            patch = images[:, row:row+kh+sh, col:col+kw+sw]
+            output[:, row, col] = np.sum(patch * kernel, axis=(1, 2))
+    return output
