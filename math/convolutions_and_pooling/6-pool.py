8d6de5abecb62548d6e04f34f3d2e5d5ba99b74a
Kamil <nezerovk425@gmail.com>
2026-03-27 23:20:23 +0400

    maxpooling

---
+++ math/convolutions_and_pooling/6-pool.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    '''pooling in CNN'''
+    h, kh = images.shape[1], kernel_shape.shape[0]
+    w, kw = images.shape[2], kernel_shape.shape[1]
+    sh, sw = stride
+    c = images.shape[3]
+    oh, ow = (int(np.floor((h - kh) / sh)) + 1,
+              int(np.floor((w - kw) / sw)) + 1)
+
+    output = np.zeros(shape=(images.shape[0], oh, ow, c))
+    for row in range(oh):
+        for col in range(ow):
+            patch = images[:, row * sh: row * sh + kh,
+                           col * sw: col * sw + kw, :]
+            output[:, row, col, :] = np.max(axis=(1, 2))
+    return output
