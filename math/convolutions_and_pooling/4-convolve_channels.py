036b7d444c3e2d632e26e2144d13c596365e43bc
Kamil <nezerovk425@gmail.com>
2026-03-27 22:42:03 +0400

    ff

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -8,7 +8,6 @@ def convolve_channels(images, kernel,
     '''convolving with channels'''
     h, kh = images.shape[1], kernel.shape[0]
     w, kw = images.shape[2], kernel.shape[1]
-    od = kernel.shape[2]
     sh, sw = stride
     if padding == "same":
         ph = int(((h - 1) * sh + kh - h) / 2) + 1
@@ -23,13 +22,11 @@ def convolve_channels(images, kernel,
     images = np.pad(images,
                     ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                     constant_values=(0))
-    output = np.zeros(shape=(images.shape[0], oh, ow, od))
+    output = np.zeros(shape=(images.shape[0], oh, ow))
     for row in range(oh):
-        c = 0
         for col in range(ow):
             patch = images[:, row * sh: row * sh + kh,
                            col * sw: col * sw + kw, :]
-            output[:, row, col, 0] = np.sum(patch * kernel,
+            output[:, row, col] = np.sum(patch * kernel,
                                             axis=(1, 2, 3))
-            c += 1
     return output
