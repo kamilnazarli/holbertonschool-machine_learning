77faca20dfceb1ee99c51b5fb4bea381fb8c1325
Kamil <nezerovk425@gmail.com>
2026-03-27 23:01:57 +0400

    first

---
+++ math/convolutions_and_pooling/5-convolve.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+    '''multiple kernels'''
+    h, kh = images.shape[1], kernels.shape[0]
+    w, kw = images.shape[2], kernels.shape[1]
+    sh, sw = stride
+    nc = kernels.shape[3]
+    if padding == "same":
+        ph = int(((h - 1) * sh + kh - h) / 2) + 1
+        pw = int(((w - 1) * sw + kw - w) / 2) + 1
+    elif padding == "valid":
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    oh, ow = (int(np.floor((h - kh + 2 * ph) / sh)) + 1,
+              int(np.floor((w - kw + 2 * pw) / sw)) + 1)
+    images = np.pad(images,
+                    ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                    constant_values=(0))
+    output = np.zeros(shape=(images.shape[0], oh, ow, nc))
+    for row in range(oh):
+        for col in range(ow):
+            patch = images[:, row * sh: row * sh + kh,
+                           col * sw: col * sw + kw, :]
+            output[:, row, col, :] = np.sum(patch * kernels,
+                                            axis=(1, 2, 3))
+    return output
