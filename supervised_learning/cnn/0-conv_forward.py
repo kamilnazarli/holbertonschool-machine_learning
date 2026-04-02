9e60d22a65c2de99ed5717dbd63ad632b7d7e11f
Kamil <nezerovk425@gmail.com>
2026-04-02 21:48:25 +0400

    fix: some syntax errors

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -16,15 +16,16 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     h_prev, w_prev = A_prev.shape[1], A_prev.shape[2]
     sh, sw = stride
     kh, kw = W.shape[0], W.shape[1]
+    m = A_prev.shape[0]
     if padding == "same":
         ph = int(((h_prev-1) * sh + kh - h_prev) / 2) + 1
         pw = int(((w_prev-1) * sw + kw - w_prev) / 2) + 1
     else:
-        ph, pw = 0
+        ph, pw = 0, 0
     output_h = int(1 + (A_prev.shape[1] + 2 * ph - W.shape[1]) / sh)
     output_w = int(1 + (A_prev.shape[2] + 2 * pw - W.shape[2]) / sw)
     output_c = W.shape[3]
-    output = np.zeros((output_h, output_w, output_c))
+    output = np.zeros((m, output_h, output_w, output_c))
     for row in range(output_h):
         for col in range(output_w):
             output[row, col, :] = np.sum(W[row, col, :, :] * A_prev[:, row, col, :], axis=(1, 2)) + b
