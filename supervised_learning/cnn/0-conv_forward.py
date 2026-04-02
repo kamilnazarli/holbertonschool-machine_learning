114c316fff98e9b76babe84db7b3eed3dbabbbd8
Kamil <nezerovk425@gmail.com>
2026-04-02 23:42:25 +0400

    pycodestyle

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -29,12 +29,14 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     A_prev = np.pad(A_prev,
                     ((0, 0), (ph, ph),
                     (pw, pw), (0, 0)),
-                    constant_values=0)
+                    constant_values = 0)
     output = np.zeros((m, output_h, output_w, output_c))
     for row in range(output_h):
         for col in range(output_w):
+            for k in range(c_new):
                 patch = A_prev[:, row * sh: row * sh + kh,
                                col * sw: col * sw + kw, :]
-                output[:, row, col, :] = (np.sum(W * patch,
-                                                  axis=(1, 2, 3)) + b)
+                output[:, row, col, k] = (np.sum(W[:, :, :, k] * patch, axis=(1, 2, 3)) +
+                                          b[:, :, :, k])
+    
     return activation(output)
