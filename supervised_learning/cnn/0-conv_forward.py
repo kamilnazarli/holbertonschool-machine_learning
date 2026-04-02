c9bc45e48cb0784388f1b117be86cbbcfb3c102f
Kamil <nezerovk425@gmail.com>
2026-04-02 21:52:12 +0400

    another fix

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -22,8 +22,8 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         pw = int(((w_prev-1) * sw + kw - w_prev) / 2) + 1
     else:
         ph, pw = 0, 0
-    output_h = int(1 + (A_prev.shape[1] + 2 * ph - W.shape[1]) / sh)
-    output_w = int(1 + (A_prev.shape[2] + 2 * pw - W.shape[2]) / sw)
+    output_h = int(1 + (A_prev.shape[1] + 2 * ph - kh) / sh)
+    output_w = int(1 + (A_prev.shape[2] + 2 * pw - kw) / sw)
     output_c = W.shape[3]
     output = np.zeros((m, output_h, output_w, output_c))
     for row in range(output_h):
