0e43ee35c208551812936b204ec373997e86617d
Kamil <nezerovk425@gmail.com>
2026-04-04 16:35:44 +0400

    tt

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -27,8 +27,8 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     m = A_prev.shape[0]
     c_new = b.shape[3]
     if padding == "same":
-        ph = int(((h_prev-1) * sh + kh - h_prev) / 2)
-        pw = int(((w_prev-1) * sw + kw - w_prev) / 2)
+        ph = int(((h_prev-1) * sh + kh - h_prev) / 2) + 1
+        pw = int(((w_prev-1) * sw + kw - w_prev) / 2) + 1
     else:
         ph, pw = 0, 0  
     output_h = int(1 + (h_prev + 2 * ph - kh) / sh)
