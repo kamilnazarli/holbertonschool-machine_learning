0564fb592140acf41025c2aa494ae23ded0a7f6b
Kamil <nezerovk425@gmail.com>
2026-04-04 18:42:03 +0400

    added seperately

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -12,14 +12,17 @@ def lenet5(X):
     model = K.Sequential()
     model.add(
         K.layers.Conv2D(6, kernel_size=(5, 5), activation="relu",
-                        padding="same", kernel_initializer="he_normal"),
-        K.layers.MaxPooling2D(kernel_size=(2, 2), strides=(2, 2)),
-        K.layers.Conv2D(16, kernel_size=(5, 5), activation="relu",
-                        padding="valid", kernel_initializer="he_normal"),
-        K.layers.MaxPooling2D(kernel_size=(2, 2), strides=(2, 2)),
-        K.layers.Dense(120, activation="relu"),
-        K.layers.Dense(84, activation="relu"),
-        K.layers.Dense(10, activation="softmax")
-    )
+                        padding="same", kernel_initializer="he_normal"))
+    model.add(K.layers.MaxPooling2D(kernel_size=(2, 2), strides=(2, 2)))
+    model.add(K.layers.Conv2D(16, kernel_size=(5, 5), activation="relu",
+                       padding="valid", kernel_initializer="he_normal"))
+    model.add(K.layers.MaxPooling2D(kernel_size=(2, 2), strides=(2, 2)))
+
+    model.add(K.layers.Flatten())
+    model.add(K.layers.Dense(120, activation="relu"))
+    model.add(K.layers.Dense(84, activation="relu"))
+    model.add(K.layers.Dense(10, activation="softmax"))
+
     model.compile(optimizer="adam",
                   metrics=["accuracy"])
+    return model
