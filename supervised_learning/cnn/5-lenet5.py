fadde9037e1aabe45eaa706f5390f58436fcdc90
Kamil <nezerovk425@gmail.com>
2026-04-04 18:48:04 +0400

    typo

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -8,12 +8,11 @@ def lenet5(X):
     X is a K.Input of shape (m, 28, 28, 1)
     containing the input images for the network
     '''
-    pass
     model = K.Sequential()
     model.add(
         K.layers.Conv2D(6, kernel_size=(5, 5), activation="relu",
                         padding="same", kernel_initializer="he_normal",
-                        input_shape=X.shape))
+                        input_shape=(28, 28, 1)))
     model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(K.layers.Conv2D(16, kernel_size=(5, 5), activation="relu",
                        padding="valid", kernel_initializer="he_normal"))
