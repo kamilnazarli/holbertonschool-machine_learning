86e295f5d6ac483c36b8d2a54e31f9692599a744
Kamil <nezerovk425@gmail.com>
2026-04-05 12:35:39 +0400

    built model on top of inputs

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -9,21 +9,21 @@ def lenet5(X):
     containing the input images for the network
     '''
     initializer = K.initializers.HeNormal(seed=0)
-    model = K.Sequential()
-    model.add(
-        K.layers.Conv2D(6, kernel_size=(5, 5), activation="relu",
-                        padding="same", kernel_initializer=initializer,
-                        input_shape=(28, 28, 1)))
-    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(K.layers.Conv2D(16, kernel_size=(5, 5), activation="relu",
-                       padding="valid", kernel_initializer=initializer))
-    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(K.layers.Flatten())
-    model.add(K.layers.Dense(120, activation="relu"))
-    model.add(K.layers.Dense(84, activation="relu"))
-    model.add(K.layers.Dense(10, activation="softmax"))
+    Z1 = K.layers.Conv2D(6, kernel_size=(5, 5), activation="relu",
+                         padding="same", kernel_initializer=initializer,
+                         input_shape=(28, 28, 1))(X)
+    P1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(Z1)
+    Z2 = K.layers.Conv2D(16, kernel_size=(5, 5), activation="relu",
+                         padding="valid",
+                         kernel_initializer=initializer)(P1)
+    P2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(Z2)
 
+    F = K.layers.Flatten()(P2)
+    D1 = K.layers.Dense(120, activation="relu")(F)
+    D2 = K.layers.Dense(84, activation="relu")(D1)
+    output = K.layers.Dense(10, activation="softmax")(D2)
+    model = K.Model(inputs=X, outputs=output)
     model.compile(optimizer="adam",
                   metrics=["accuracy"])
     return model
