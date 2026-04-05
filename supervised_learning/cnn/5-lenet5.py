eda262ee531d83e232017adafe1a6e472509b6ce
Kamil <nezerovk425@gmail.com>
2026-04-05 12:49:48 +0400

    fixed pycodestyle errors

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -17,7 +17,8 @@ def lenet5(X):
     P1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(Z1)
     Z2 = K.layers.Conv2D(16, kernel_size=(5, 5), activation="relu",
                          padding="valid",
-                         kernel_initializer=K.initializers.HeNormal(seed=0))(P1)
+                         kernel_initializer=K.initializers.HeNormal(seed=0))
+    Z2 = Z2(P1)
     P2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(Z2)
 
     F = K.layers.Flatten()(P2)
@@ -26,7 +27,8 @@ def lenet5(X):
     D2 = K.layers.Dense(84, activation="relu",
                         kernel_initializer=K.initializers.HeNormal(seed=0))(D1)
     output = K.layers.Dense(10, activation="softmax",
-                            kernel_initializer=K.initializers.HeNormal(seed=0))(D2)
+                            kernel_initializer=K.initializers.HeNormal(seed=0))
+    output = output(D2)
     model = K.Model(inputs=X, outputs=output)
     model.compile(loss="categorical_crossentropy",
                   optimizer="adam",
