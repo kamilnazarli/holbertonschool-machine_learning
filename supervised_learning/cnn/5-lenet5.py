4b18030ee08288e744f6f9744c22f3d919e67914
Kamil <nezerovk425@gmail.com>
2026-04-05 12:42:59 +0400

    initializer added to all layers

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -11,7 +11,8 @@ def lenet5(X):
     initializer = K.initializers.HeNormal(seed=0)
 
     Z1 = K.layers.Conv2D(6, kernel_size=(5, 5), activation="relu",
-                         padding="same", kernel_initializer=initializer,
+                         padding="same",
+                         kernel_initializer=initializer,
                          input_shape=(28, 28, 1))(X)
     P1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(Z1)
     Z2 = K.layers.Conv2D(16, kernel_size=(5, 5), activation="relu",
@@ -20,9 +21,12 @@ def lenet5(X):
     P2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(Z2)
 
     F = K.layers.Flatten()(P2)
-    D1 = K.layers.Dense(120, activation="relu")(F)
-    D2 = K.layers.Dense(84, activation="relu")(D1)
-    output = K.layers.Dense(10, activation="softmax")(D2)
+    D1 = K.layers.Dense(120, activation="relu",
+                        kernel_initializer=initializer)(F)
+    D2 = K.layers.Dense(84, activation="relu",
+                        kernel_initializer=initializer)(D1)
+    output = K.layers.Dense(10, activation="softmax",
+                            kernel_initializer=initializer)(D2)
     model = K.Model(inputs=X, outputs=output)
     model.compile(loss="categorical_crossentropy",
                   optimizer="adam",
