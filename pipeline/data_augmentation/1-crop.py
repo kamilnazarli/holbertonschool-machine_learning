f9a9ef7355eb09136dd327d311aabb38405891c1
Kamil <nezerovk425@gmail.com>
2026-04-10 11:43:23 +0400

    cropping an image

---
+++ pipeline/data_augmentation/1-crop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''module documented'''
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    '''image is a 3D tf.Tensor 
+       containing the image to crop
+       size is a tuple containing
+       the size of the crop'''
+    res = tf.image.crop_and_resize(image,
+                                   crop_size=size)
+    return res
