e6cbb8266cb06c60c206652d75f534b2416a6280
Kamil <nezerovk425@gmail.com>
2026-04-10 21:00:41 +0400

    adjusting hue

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -9,6 +9,6 @@ def change_hue(image, delta):
     containing the image to change
     delta is the amount the hue should change
     '''
-    img = tf.image.random_hue(image,
+    img = tf.image.adjust_hue(image,
                                delta)
     return img
