fe6a577bf2292900777340041fc4175b99e5e207
Kamil <nezerovk425@gmail.com>
2026-05-03 16:14:57 +0400

    fix: changed completely

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -4,16 +4,23 @@ import numpy as np
 import tensorflow.keras as K
 
 
-class Yolo(K.Model):
-    """Yolo v3 object detection model."""
-
+class Yolo:
+    '''yolo class'''
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """Initialize the Yolo model."""
-        super().__init__()
+        '''
+        -model_path is the path to where a Darknet Keras model is stored
+        -classes_path is the path to where the list of class names used
+        for the Darknet model, listed in order of index, can be found
+        -class_t is a float representing the box score threshold for the
+        initial filtering step
+        -nms_t is a float representing the IOU threshold for non-max
+        suppression
+        -anchors is a numpy.ndarray of shape (outputs, anchor_boxes, 2)
+        containing all of the anchor boxes
+        '''
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as f:
-            class_names = [line.strip() for line in f]
-        object.__setattr__(self, 'class_names', class_names)
+        with open(classes_path, "r") as f:
+            self.class_names = f.read().split()
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
