098a8cfdb584fd8608a69523d994397ba4ed0ca7
Kamil <nezerovk425@gmail.com>
2026-05-17 14:06:24 +0400

    fix: resize shapes

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -10,12 +10,12 @@ def pca(X, ndim):
     - d is the number of dimensions in each point
     - ndim is the new dimensionality of the transformed X
     '''
-    n = X.shape[0]
+    n, d = X.shape[0], X.shape[1]
     X_centered = X - np.mean(X, axis=0)
     #  (n, n) (n, d) (d, d)
     U, S, Vt = np.linalg.svd(X_centered)
 
     # r = np.argmax(variance_cum >= var) + 1
     Wk = Vt.T
-    X_transformed = np.resize(Wk, (n, ndim))
+    X_transformed = np.resize(Wk, (d, ndim))
     return X_transformed
