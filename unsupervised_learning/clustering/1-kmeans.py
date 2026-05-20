88ae19c93a6dd606ac2383372489c2219a403662
Kamil <nezerovk425@gmail.com>
2026-05-20 17:05:21 +0400

    implementation of K-means

---
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+'''clustering'''
+import numpy as np
+
+
+def initialize(X, k):
+    '''- X is a numpy.ndarray of shape (n, d) containing the dataset
+       that will be used for K-means clustering
+       - n is the number of data points
+       - d is the number of dimensions for each data point
+       - k is a positive integer containing the number of clusters'''
+    if not (isinstance(X, np.ndarray) and X.ndim == 2):
+        return None
+    n, d = X.shape
+    if not (isinstance(k, int) and k <= n and k > 0):
+        return None
+    min_val = np.min(X, axis=0)
+    max_val = np.max(X, axis=0)
+    cluster_centroids = np.random.uniform(low=min_val,
+                                          high=max_val,
+                                          size=(k, d))
+    return cluster_centroids
+
+def kmeans(X, k, iterations=1000):
+    '''- X is a numpy.ndarray of shape (n, d) containing the dataset
+       - n is the number of data points
+       - d is the number of dimensions for each data point
+       - k is a positive integer containing the number of clusters
+       - iterations is a positive integer containing the maximum
+       number of iterations that should be performed'''
+    n, d = X.shape
+    min_val = np.min(X, axis=0)
+    max_val = np.max(X, axis=0)
+    cluster_centroids = np.random.uniform(low=min_val,
+                                          high=max_val,
+                                          size=(k, d))
+    clss = np.zeros(n,)
+    for i in range(iterations):
+        # (n, k)
+        distances = np.sqrt(
+            np.sum((X[:, np.newaxis] - cluster_centroids) ** 2, axis=(2)))
+        clss = np.argmin(distances, axis=1)
+        old_c = cluster_centroids.copy()
+        for j in range(k):
+            if len(np.where(clss == j)):
+                cluster_centroids[j, :] = (
+                    np.mean(X[np.where(clss == j)], axis=0))
+            else:
+                cluster_centroids[j, :] = (
+                    np.random.uniform(low=min_val,
+                    high=max_val,
+                    size=(1, d)))
+        if np.all(cluster_centroids == old_c):
+            return
+    return cluster_centroids, clss
+        
