5afafd76403d2d3ffb72fa802e425754ba8fd7cb
Kamil <nezerovk425@gmail.com>
2026-05-22 14:15:05 +0400

    tt

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -9,7 +9,11 @@ def variance(X, C):
     - C is a numpy.ndarray of shape (k, d)
     containing the centroid means for each cluster
     '''
+    if not (isinstance(X, np.ndarray) and X.ndim == 2):
+        return None
+    if not (isinstance(C, np.ndarray) and C.ndim == 2):
+        return None
     n = X.shape[0]
-    var = np.sum((X[:, np.newaxis] - C) ** 2, axis=(2, 0)) / n
+    var = np.sum(np.sum((X[:, np.newaxis] - C) ** 2, axis=(2, 0)) / n)
     return var
 
