3c5cbabbf0da84b068d976bbf5c4322d2d7b0606
Kamil <nezerovk425@gmail.com>
2026-05-26 13:23:40 +0400

    feat: catch invalid cases

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -16,6 +16,17 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     - iterations is a positive integer containing the
     maximum number of iterations for K-means
     '''
+    if not (isinstance(X, np.ndarray) and X.ndim == 2):
+        return None, None
+    n, d = X.shape
+    if not (isinstance(kmin, int) and kmin > 1):
+        return None, None
+    if not (isinstance(kmax, int) and kmax > 0):
+        return None, None
+    if kmin > kmax:
+        return None, None
+    if not (isinstance(iterations, int) and iterations > 0):
+        return None, None
     results, d_vars = [], []
     for k in range(kmin, kmax + 1):
         C, out = kmeans(X, k)
