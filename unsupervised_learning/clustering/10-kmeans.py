5946427292aa4cbdc4486d479bd6af58a55b465b
Kamil <nezerovk425@gmail.com>
2026-05-26 16:39:24 +0400

    Implementing Kmeans using sklearn

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''clustering'''
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    '''
+    - X is a numpy.ndarray of shape (n, d)
+    containing the dataset
+    - k is the number of clusters
+    '''
+    km = cluster.KMeans(n_clusters=k).fit(X)
+    return km.cluster_centers_, km.labels_
