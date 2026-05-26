992f94120a1dca31e47cd831d2b637aaa764f391
Kamil <nezerovk425@gmail.com>
2026-05-26 16:26:56 +0400

    Implement maximization

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Maximization step in the EM algorithm for a GMM"""
+import numpy as np
+
+
+def maximization(X, g):
+    """Calculates the maximization step in the EM algorithm for a GMM.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        g: numpy.ndarray of shape (k, n) containing posterior probabilities
+
+    Returns:
+        pi, m, S or None, None, None on failure
+    """
+    if not (isinstance(X, np.ndarray) and X.ndim == 2):
+        return None, None
+    if not (isinstance(g, np.ndarray) and g.ndim == 2):
+        return None, None
+    n, d = X.shape
+    k = g.shape[0]
+    if g.shape[1] != n:
+        return None, None, None
+    if not np.isclose(np.sum(g, axis=0), 1).all():
+        return None, None, None
+    Nk = np.sum(g, axis=1)
+    pi = Nk / n
+    m = (g @ X) / Nk[:, np.newaxis]
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        diff = X - m[i]
+        S[i] = (g[i, :, np.newaxis] * diff).T @ diff / Nk[i]
+    return pi, m, S
