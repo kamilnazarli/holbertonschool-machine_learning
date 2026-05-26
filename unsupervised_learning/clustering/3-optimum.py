cd11552900378955778892fd69143893f3b34474
Kamil <nezerovk425@gmail.com>
2026-05-26 13:17:48 +0400

    optimizing K

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''clustering'''
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    '''
+    - X is a numpy.ndarray of shape (n, d) containing
+    the data set
+    - kmin is a positive integer containing the minimum
+    number of clusters to check for (inclusive)
+    - kmax is a positive integer containing the maximum
+    number of clusters to check for (inclusive)
+    - iterations is a positive integer containing the
+    maximum number of iterations for K-means
+    '''
+    results, d_vars = [], []
+    for k in range(kmin, kmax + 1):
+        C, out = kmeans(X, k)
+        results.append(out)
+        d_vars.append(variance(X, C))
+    return results, d_vars
