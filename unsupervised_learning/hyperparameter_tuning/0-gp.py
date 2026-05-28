2f3017429fccefdc134403318400a1301f5c4090
Kamil <nezerovk425@gmail.com>
2026-05-28 14:39:41 +0400

    first commit

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+'''hyperparameter tuning'''
+import numpy as np
+
+
+class GaussianProcess:
+    ''' noiseless 1D Gaussian process'''
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        '''
+        - X_init is a numpy.ndarray of shape (t, 1) representing
+        the inputs already sampled with the black-box function
+        - Y_init is a numpy.ndarray of shape (t, 1) representing
+        the outputs of the black-box function for each input in X_init
+        - t is the number of initial samples
+        - l is the length parameter for the kernel
+        - sigma_f is the standard deviation given to the output
+        of the black-box function'''
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.Y)
+
+    def kernel(self, X1, X2):
+        '''
+        X1 is a numpy.ndarray of shape (m, 1)
+        X2 is a numpy.ndarray of shape (n, 1)
+        '''
+        dist = - (X1 - X2) ** 2
+        exponential = np.exp(dist / (2 * self.l ** 2))
+        k_rbf = (self.sigma_f ** 2) * exponential
+        return k_rbf
