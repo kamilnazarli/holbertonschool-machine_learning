5b98a8d7519c44e562b70ce864866768344f3918
Kamil <nezerovk425@gmail.com>
2026-05-30 18:55:55 +0400

    Optimize black-box function

---
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+'''hyperparameter tuning'''
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    '''
+    Bayesian optimization on a noiseles
+    1D Gaussian process
+    '''
+    def __init__(self, f, X_init, Y_init, bounds,
+                 ac_samples, l=1, sigma_f=1,
+                 xsi=0.01, minimize=True):
+        '''
+        - f is the black-box function to be optimized
+        - X_init is a numpy.ndarray of shape (t, 1) representing
+        the inputs already sampled with the black-box function
+        - Y_init is a numpy.ndarray of shape (t, 1) representing the
+        outputs of the black-box function for each input in X_init
+        - t is the number of initial samples
+        - bounds is a tuple of (min, max) representing the bounds of
+        the space in which to look for the optimal point
+        - ac_samples is the number of samples that should be analyzed
+        during acquisition
+        - l is the length parameter for the kernel
+        - sigma_f is the standard deviation given to the output of the
+        black-box function
+        - xsi is the exploration-exploitation factor for acquisition
+        - minimize is a bool determining whether optimization should be
+        performed for minimization (True) or maximization (False)
+
+        '''
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        '''
+        calculates the next best sample location
+        '''
+        mu, sigma = self.gp.predict(self.X_s)
+        sigma = sigma.reshape(-1)
+        mu = mu.reshape(-1)
+        if self.minimize:
+            best = np.min(self.gp.Y)
+            improvement = best - mu - self.xsi
+        else:
+            best = np.max(self.gp.Y)
+            improvement = mu - best - self.xsi
+        EI = np.zeros_like(mu)
+        mask = sigma > 0
+        Z = np.zeros_like(mu)
+        Z[mask] = (improvement[mask] / sigma[mask])
+        cdf_m = improvement[mask] * norm.cdf(Z[mask])
+        pdf_m = sigma[mask] * norm.pdf(Z[mask])
+        EI[mask] = cdf_m + pdf_m
+        X_next = self.X_s[np.argmax(EI)]
+        return X_next, EI
+
+    def optimize(self, iterations=100):
+        '''
+        optimizes the black-box function
+        '''
+        for _ in range(iterations):
+            X_next, _ = self.acquisition()
+
+            if np.any(np.isclose(X_next, self.gp.X)):
+                break
+            Y_next = self.f(X_next)
+            self.gp.update(
+                X_next.reshape(1, 1),
+                np.array([[Y_next]])
+            )
+
+        if self.minimize:
+            idx = np.argmin(self.gp.Y)
+        else:
+            idx = np.argmax(self.gp.Y)
+
+        return self.gp.X[idx], self.gp.Y[idx]
