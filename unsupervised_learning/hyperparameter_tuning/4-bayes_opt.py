6b823e2cadaa4532f89f4892bd0dc2d080fc4823
Kamil <nezerovk425@gmail.com>
2026-05-30 18:46:34 +0400

    pep 8

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -54,9 +54,8 @@ class BayesianOptimization:
         EI = np.zeros_like(mu)
         nonzero_sigma = sigma > 0
         Z = np.zeros_like(mu)
-        Z[nonzero_sigma] = (
-            improvement[nonzero_sigma] / sigma[nonzero_sigma])
-        EI[nonzero_sigma] = (improvement[nonzero_sigma] * norm.cdf(Z[nonzero_sigma]) +
-                             sigma[nonzero_sigma] * norm.pdf(Z[nonzero_sigma]))
+        Z[nonzero_sigma] = (improvement[nonzero_sigma] / sigma[nonzero_sigma])
+        EI[nonzero_sigma] = (improvement[nonzero_sigma] * norm.cdf(Z[nonzero_sigma])
+                             + sigma[nonzero_sigma] * norm.pdf(Z[nonzero_sigma]))
         X_next = self.X_s[np.argmax(EI)]
         return X_next, EI
