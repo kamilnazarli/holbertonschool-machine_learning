0e0f6efe1616c15136bbc076c7d67ddd738a87d5
Kamil <nezerovk425@gmail.com>
2026-06-04 16:49:07 +0400

    fix: typo

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -20,14 +20,14 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         auto: the full autoencoder model
 
     '''
-    global_input = keras.layers.InputLayer(input_shape=(input_dims,))
+    global_input = keras.layers.Input(shape=(input_dims,))
     x = global_input
     for layer in hidden_layers:
         x = keras.layers.Dense(layer, activation="relu")(x)
     latent_space = keras.layers.Dense(latent_dims, activation="relu")(x)
     encoder = keras.models.Model(inputs=global_input, outputs=latent_space)
 
-    y = keras.layers.InputLayer(input_shape=(latent_dims,))
+    y = keras.layers.Input(shape=(latent_dims,))
     for layer in hidden_layers[::-1]:
         y = keras.layers.Dense(layer, activation="relu")(y)
     decoder_output = keras.layers.Dense(input_dims, activation="sigmoid")(y)
