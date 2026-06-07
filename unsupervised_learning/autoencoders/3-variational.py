4669b32e428b01c5479f0d16b7449dc3a9faad04
Kamil <nezerovk425@gmail.com>
2026-06-07 18:09:52 +0400

    VAE

---
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+'''autoencoders'''
+import tensorflow.keras as keras
+
+
+class Sampling(keras.layers.layer):
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = keras.shape(z_mean)[0]
+        dim = keras.shape(z_mean)[1]
+        epsilon = keras.random.normal(shape=(batch, dim))
+        return z_mean + keras.exp(0.5 * z_log_var) * epsilon
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    '''
+    This function creates an autoencoder.
+
+    Args:
+        input_dims(int): containing the dimensions input
+        hidden_layers(list): containing the number of nodes
+            for each hidden layer in the encoder, respectively
+        latent_dims(int): containing the dimensions of the
+            latent space representation
+
+    Returns:
+        encoder: the encoder model
+        decoder: the decoder model
+        auto: the full autoencoder model
+
+    '''
+    global_input = keras.layers.Input(shape=(input_dims,))
+    x = global_input
+    for layer in hidden_layers:
+        x = keras.layers.Dense(layer, activation="relu")(x)
+    latent_space = keras.layers.Dense(latent_dims, activation="relu")(x)
+    z_mean = keras.layers.Dense(latent_dims, name="z_mean")(x)
+    z_log_var = keras.layers.Dense(latent_dims, name="z_log_var")(x)
+    z = Sampling(name="z")([z_mean, z_log_var])
+    encoder = keras.models.Model(inputs=global_input,
+                                 outputs=[z_mean, z_log_var, z],
+                                 name="encoder")
+    decoder_input = keras.layers.Input(shape=(latent_dims,))
+    y = decoder_input
+    for layer in hidden_layers[::-1]:
+        y = keras.layers.Dense(layer, activation="relu")(y)
+    decoder_output = keras.layers.Dense(input_dims, activation="sigmoid")(y)
+    decoder = keras.models.Model(inputs=decoder_input, outputs=decoder_output)
+
+    auto_output = decoder(encoder(global_input))
+    auto = keras.models.Model(inputs=global_input, outputs=auto_output)
+    auto.compile(
+        optimizer="adam",
+        loss="binary_crossentropy"
+    )
+    return encoder, decoder, auto
