03bdde07a34959e4d164c401bebea8acfa138bf4
Kamil <nezerovk425@gmail.com>
2026-06-24 16:29:56 +0400

    fix: method gets last hidden state

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -10,9 +10,13 @@ def rnn(rnn_cell, X, h_0):
     - h_0 is the initial hidden state,
     given as a numpy.ndarray of shape (m, h)
     """
+    # _, m, i = X.shape
+    # _, h = h_0.shape
+    # cell = rnn_cell(i, h, o)
     H, Y = [h_0], []
+    h_current = h_0
     for x_t in X:
-        h_next, y = rnn_cell.forward(h_0, x_t)
-        H.append(h_next)
+        h_current, y = rnn_cell.forward(h_current, x_t)
+        H.append(h_current)
         Y.append(y)
     return np.array(H), np.array(Y)
